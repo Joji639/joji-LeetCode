94dfa8ce72c2c5ca11dfda00f1880ddcd488e6a7
Joji639 <Jojijoj367@gmail.com>
2025-10-25 14:58:11 +0530

    Time: 0 ms (100%), Space: 56.5 MB (5.22%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,7 +4,12 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-   
+   for(let i =0; i<nums.length;i++) {
+       if(nums[i]===target){
+           return i 
+       }
+       
+   }
          nums.push(target)
          nums.sort((a,b)=>a-b)
          return nums.indexOf(target)
