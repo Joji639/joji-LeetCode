38539625fd9231d8ba67e1d4c72ff4cef34ee3b8
Joji639 <Jojijoj367@gmail.com>
2025-10-27 09:56:30 +0530

    Time: 1 ms (57.03%), Space: 55.1 MB (77.78%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.js
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    let count=0;
+    let a =0;
     for(let i =0; i<s.length-1; i++){
-        count+=Math.abs((s.charCodeAt(i))-(s.charCodeAt(i+1)))
+            a+=Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1))
+            
+        }
+    return a 
     }
-    return count
-};
