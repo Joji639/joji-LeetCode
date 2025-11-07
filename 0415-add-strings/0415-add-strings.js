05a6fd121d58096d04b1748caf875558886afc26
Joji639 <Jojijoj367@gmail.com>
2025-11-07 10:11:32 +0530

    Time: 1 ms (86.19%), Space: 56.9 MB (27.76%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let a =BigInt(num1)
+    let b = BigInt(num2)
+    a+=b
+    return String(a)
+};
